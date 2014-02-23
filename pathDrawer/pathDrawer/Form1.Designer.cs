cc8a8dce822a6b8ce3bf329dc89114b8755c6d20
Daniil <ivanov-daniil93@mail.ru>
2014-02-23 23:09:18 +0400

    PathDrawer app

---
+++ pathDrawer/pathDrawer/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace pathDrawer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.coordinateBar = new System.Windows.Forms.StatusStrip();
+            this.coordShow = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.coordinateBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.coordinateBar, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 82.42812F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.57188F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(558, 271);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tableLayoutPanel1.SetColumnSpan(this.pictureBox1, 2);
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(552, 217);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Layout += new System.Windows.Forms.LayoutEventHandler(this.pictureBox1_Layout);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // coordinateBar
+            // 
+            this.coordinateBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.coordinateBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.coordShow});
+            this.coordinateBar.Location = new System.Drawing.Point(0, 223);
+            this.coordinateBar.Name = "coordinateBar";
+            this.coordinateBar.Size = new System.Drawing.Size(279, 48);
+            this.coordinateBar.TabIndex = 1;
+            this.coordinateBar.Text = "Tada";
+            // 
+            // coordShow
+            // 
+            this.coordShow.Name = "coordShow";
+            this.coordShow.Size = new System.Drawing.Size(10, 43);
+            this.coordShow.Text = ";";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 271);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Drawer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.coordinateBar.ResumeLayout(false);
+            this.coordinateBar.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip coordinateBar;
+        private System.Windows.Forms.ToolStripStatusLabel coordShow;
+    }
+}
+
---
+++ pathDrawer/pathDrawer/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pathDrawer
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap pic = new Bitmap(1,1);
+        game.Agent bill = new game.Agent();
+        private double desX = 0.01;
+        private double desY = 0.01;
+
+        public Form1()
+        {
+            InitializeComponent();
+            pic = new Bitmap(pictureBox1.Size.Width, pictureBox1.Size.Height);
+
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            drawCircle(ref pic, e.X, e.Y, Color.Green, 2);
+            desX = e.X;
+            desY = e.Y;
+        }
+
+        private void pictureBox1_Layout(object sender, LayoutEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            pic = new Bitmap(pic, pictureBox1.Size);
+
+            int x, y;
+            x = (int)(bill.getX() * pictureBox1.Size.Width);
+            y = (int)(bill.getY() * pictureBox1.Size.Height);
+            pic.SetPixel(x, y, Color.Blue);
+            //drawCircle(ref pic, x, y, Color.Blue, 2);
+
+            double dirX = (desX / pictureBox1.Size.Width) - bill.getX();
+            double dirY = (desY / pictureBox1.Size.Height) - bill.getY();
+            double norm = Math.Sqrt(dirX * dirX + dirY * dirY);
+            dirX /= norm;
+            dirY /= norm;
+
+            bill.move(dirX, dirY);
+
+            x = (int)(bill.getX() * pictureBox1.Size.Width);
+            y = (int)(bill.getY() * pictureBox1.Size.Height);
+            pic.SetPixel(x, y, Color.Black);
+           // drawCircle(ref pic, x, y, Color.Black, 2);
+
+            pictureBox1.BackgroundImage = pic;
+
+            coordShow.Text = desX.ToString() + ";" + desY.ToString();
+        }
+        double radius;
+        private void drawCircle(ref Bitmap pic, int x, int y, Color col, int rad)
+        {
+            for (int i = Math.Max(1, x - rad); i < Math.Min(pic.Size.Width, x + rad); i++)
+            {
+                for (int j = Math.Max(1, y - rad); j < Math.Min(pic.Size.Height, y + rad); j++)
+                {
+                    i -= x;
+                    j -= y;
+
+                    radius = Math.Sqrt(i * i + j * j);
+
+                    i += x;
+                    j += y;
+
+
+                    if (radius <= rad)
+                    {
+                        pic.SetPixel(i, j, col);
+                    }
+                }
+            }
+        }
+    }
+}
+
+namespace game
+{
+    public class Agent {
+        private double x=0.5;
+        private double y;
+        private double speed;
+        
+        private double desX;
+        private double desY;
+
+        public double getX()
+        {
+            return x;
+        }
+
+        public double getY()
+        {
+            return y;
+        }
+
+        public Agent () {
+            x = 0.5;
+            y = 0.5;
+            speed = 0.01;
+        }
+
+        public void setDes(double x_ , double y_) {
+            desX = x_;
+            desY = y_;
+        }
+        public bool move(double x_, double y_) {
+            x += x_ * speed;
+            y += y_ * speed;
+            if ((x >= 0.0) && (x <= 1.0) && (y >= 0.0) && (y <= 1.0))
+            {
+                return true;
+            }
+            else
+            {
+                x -= x_ * speed;
+                y -= y_ * speed;
+                return false;
+            }
+        }
+    }
+}
