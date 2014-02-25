4ffd11fe5eb11837972352b5f48cdbcb9f6b3ce2
Daniil <ivanov-daniil93@mail.ru>
2014-02-25 23:52:03 +0400

    AgentSpace programm

---
+++ agentSpace/agentSpace/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace agentSpace
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.Field = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Field)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.Field, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 81.32912F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 18.67089F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(616, 316);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // Field
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.Field, 2);
+            this.Field.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Field.Location = new System.Drawing.Point(3, 3);
+            this.Field.Name = "Field";
+            this.Field.Size = new System.Drawing.Size(610, 250);
+            this.Field.TabIndex = 0;
+            this.Field.TabStop = false;
+            this.Field.Click += new System.EventHandler(this.Field_Click);
+            this.Field.Paint += new System.Windows.Forms.PaintEventHandler(this.Field_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Location = new System.Drawing.Point(3, 256);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(302, 60);
+            this.label1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 316);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "mainForm";
+            this.Text = "Agent land";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Field)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox Field;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ agentSpace/agentSpace/Form1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace agentSpace
+{
+    public partial class mainForm : Form
+    {
+        public mainForm()
+        {
+            InitializeComponent();
+            Agent bill = new Agent(0.1f,0.6f);
+            list[0] = bill;
+            Board phirexia = new Board(ref Field, ref list);
+            label1.Text = label1.Text + "\nphirexia.agentCoord(0) :" + phirexia.agentCoord(0);
+
+            bill.setCoord(0.4f, 0.6f);
+
+            label1.Text =label1.Text + "\nbill.getCoord() : " + bill.getCoord().x.ToString() + " ; " + bill.getCoord().y.ToString();
+
+            label1.Text = label1.Text + "\nphirexia.agentCoord(0) :" + phirexia.agentCoord(0);
+           // bill.draw(ref Field);
+        }
+
+        Agent[] list = new Agent[1];
+        private void Field_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Field_Paint(object sender, PaintEventArgs e)
+        {
+            list[0].draw(e);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            float x = list[0].getCoord().x;
+            float y = list[0].getCoord().y;
+            x = (x + 0.01f)%1;
+            y = (y + 0.01f)%1;
+            list[0].setCoord(x, y);
+            Field.Refresh();
+        }
+    }
+
+    public class Board
+    {
+        private PictureBox agentWorld;
+        private Agent[] agentList;
+
+        public Board(ref PictureBox pic, ref Agent[] list)
+        {
+            agentWorld = pic;
+            agentList = list;
+        }
+
+        public string agentCoord(int agentID)
+        {
+            return agentList[agentID].getCoord().x.ToString() +" ; " + agentList[agentID].getCoord().y.ToString();
+        }
+    }
+
+
+    public class Agent :Element
+    {
+        private Coordinates coord;
+
+        public Agent()
+        {
+            coord = new Coordinates(0.5f, 0.5f);
+        }
+
+        public Agent(float x, float y)
+        {
+            coord = new Coordinates(x, y);
+        }   
+
+
+        public Coordinates getCoord () {
+            return coord;
+        }
+
+        public void setCoord(float x, float y)
+        {
+            coord.x = x;
+            coord.y = y;
+        }
+
+        public void setCoord(Coordinates cor)
+        {
+            coord = cor;
+        }
+
+        new public void draw(PaintEventArgs e) 
+        {
+            int x = (int)(coord.x * (610));
+            int y = (int)(coord.y * (250));
+            Pen p = new Pen(Color.Black,3);
+            e.Graphics.DrawEllipse(p, x, y, 5, 5);
+            
+            //dispose pen and graphics object
+            p.Dispose();
+        }
+    }
+
+
+    public class Coordinates
+    {
+        public float x { get; set; }
+        public float y { get; set; }
+        public Coordinates (float x_, float y_) {
+            x = Math.Max(Math.Min(x_, 1.0f), 0.0f);
+            y = Math.Max(Math.Min(y_, 1.0f), 0.0f);
+        }
+
+    }
+
+    public class Element
+    {
+        public void draw(PaintEventArgs e)
+        {
+        }
+    }
+}
+
+
+
+
+
