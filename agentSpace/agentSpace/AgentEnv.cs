ff986c94fb4a991818967bfd0871b42c49d69822
Daniil <ivanov-daniil93@mail.ru>
2014-04-26 00:00:19 +0400

    Создан AgentEnv
    Базовые конструкторы AgentEnv

---
+++ agentSpace/agentSpace/AgentEnv.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace agentSpace
+{ 
+    class AgentEnv
+    //This class stores properties of agent
+    {
+        Coordinates coord;
+        float speed;
+        Board server;
+        static Random randGen = new Random();
+
+        public AgentEnv()
+        {
+            float x = (float) randGen.NextDouble();
+            float y = (float) randGen.NextDouble();
+            coord = new Coordinates(x, y);
+            speed = 0.02f;
+        }
+
+        public AgentEnv(float x, float y, float speed_)
+        {
+            coord = new Coordinates(x, y);
+            speed = speed_;
+        }
+    }
+}
