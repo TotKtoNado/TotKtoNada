3279ba8c6d893d063ed1f6b628f852f8aa565aa7
Daniil <ivanov-daniil93@mail.ru>
2014-09-20 00:33:28 +0400

    Revert "Coordinates.randCoord()"
    This reverts commit fa8d8e33099ca9529a251501a8281611513c6839.

--- agentSpace/agentSpace/Form1.cs
+++ agentSpace/agentSpace/Form1.cs
@@ -22,15 +22,11 @@ namespace agentSpace
             Coordinates pos = new Coordinates(0.2f, 0.2f);
             //bill1 = board.createDummy1();
             AgentEnv bill2 = board.createFinder1();
-            AgentEnv nancy;
-            for (int i = 0; i < 10; i++)
-            {
-                nancy = board.createLittleGirl1();
-                nancy.setCoord(Coordinates.randomCoord());
-            }
+            AgentEnv nancy = board.createLittleGirl1(); 
             //bill1.setCoord(pos);
             bill2.setCoord(pos* 4.9f);
             AgentEnv iggy;
+            nancy.setCoord(new Coordinates(0.49f, 0.51f));
             //Console.WriteLine(pos.norm().ToString());
             for (int i = 0; i < 5; i++)
             {
